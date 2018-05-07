1d501f4a7753b320d7976b60a77253b845972f78
XiaYaLing <only_xyl@163.com>
2018-05-07 11:57:12 +0800

    修改config-server

--- service_ribbon/src/main/java/xyl/bmsmart/service_ribbon/controller/HiRibbonController.java
+++ service_ribbon/src/main/java/xyl/bmsmart/service_ribbon/controller/HiRibbonController.java
@@ -1,6 +1,7 @@
 package xyl.bmsmart.service_ribbon.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class HiRibbonController {
     @Resource
     HiRibbonService hiRibbonService;
 
-//    @Value("${cloud}")
+    @Value("${cloud}")
     String cloud;
 
     @RequestMapping(value = "/hi/{param}")
