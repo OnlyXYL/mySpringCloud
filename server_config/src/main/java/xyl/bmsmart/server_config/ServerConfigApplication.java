d80172d4f2cb30245a280db30f76abeab6568cfa
XiaYaLing <only_xyl@163.com>
2018-05-07 19:06:56 +0800

    修改

--- server_config/src/main/java/xyl/bmsmart/server_config/ServerConfigApplication.java
+++ server_config/src/main/java/xyl/bmsmart/server_config/ServerConfigApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ServerConfigApplication {
