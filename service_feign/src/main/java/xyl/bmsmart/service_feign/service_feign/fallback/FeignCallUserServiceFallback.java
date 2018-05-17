7f103529cb7ab34becb818ee0be0b0475cd37b45
XiaYaLing <only_xyl@163.com>
2018-05-17 20:18:40 +0800

    增加异常传递验证，未完成

--- service_feign/src/main/java/xyl/bmsmart/service_feign/service_feign/fallback/FeignCallUserServiceFallback.java
+++ service_feign/src/main/java/xyl/bmsmart/service_feign/service_feign/fallback/FeignCallUserServiceFallback.java
@@ -1,12 +1,15 @@
 package xyl.bmsmart.service_feign.service_feign.fallback;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import xyl.bmsmart.service_feign.service_feign.service.CallUserService;
 
 @Component
+@Slf4j
 public class FeignCallUserServiceFallback implements CallUserService {
     @Override
     public String getUser(String param) {
+        log.info("+++++++FeignCallUserServiceFallback++++++++++");
         return "something wrong in getting user by param";
     }
 }
