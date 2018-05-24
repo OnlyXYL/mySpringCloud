e80ee38d0b28b9edd272c53423d4a8639f17f6d0
XiaYaLing <only_xyl@163.com>
2018-05-24 10:42:00 +0800

    增加user服务

---
+++ service_ribbon/src/main/java/xyl/bmsmart/service_ribbon/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package xyl.bmsmart.service_ribbon.service.impl;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+import xyl.bmsmart.common.common.exception.CIBaseException;
+import xyl.bmsmart.service_ribbon.service.UserService;
+
+import javax.annotation.Resource;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "someThingError", ignoreExceptions = CIBaseException.class)
+    @Override
+    public String getUser(@PathVariable("param") String param) {
+
+        return restTemplate.getForObject("http://service-8762/user/{param}", String.class);
+    }
+
+    public String someThingError(String param, Throwable throwable) {
+
+        Throwable cause = throwable.getCause();
+        String message = throwable.getMessage();
+        log.info("获取服务中抛出的异常:" + message);
+
+        return message;
+    }
+}
